0253bb8d3615d50bc17c5a8be7d757f8b3234daa
Priyanshu <priyanshu@voiceoc.com>
2025-04-17 22:59:44 +0530

    removed backend

--- backend/routes/user.routes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { logIn, register } from "../controller/user.controller.js";
-const router = express.Router();
-
-router.route("/register").post(register);
-router.route("/register").post(logIn);
