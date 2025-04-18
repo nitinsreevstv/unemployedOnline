a8844c03438af2559732ec5c535e79b0fba44d17
Priyanshu <priyanshu@voiceoc.com>
2025-04-19 00:34:38 +0530

    new backend

--- WebScrapper/webscraper_api.py
+++ WebScrapper/webscraper_api.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://your-frontend-domain.com"],
+    allow_origins=["https://websrapper.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
