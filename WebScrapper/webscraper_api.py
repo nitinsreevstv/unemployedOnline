7b3e296c2e8fe9fbe28d99702d52e3d18c736d7f
Priyanshu <priyanshu@voiceoc.com>
2025-04-19 00:31:47 +0530

    added backend

--- WebScrapper/webscraper_api.py
+++ WebScrapper/webscraper_api.py
@@ -21,10 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500"
-    ],
+    allow_origins=["https://your-frontend-domain.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
